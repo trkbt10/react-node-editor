e22a4c715d35d0f08bfa5ffc207c0ef1fb4c6e14
trkbt10 <triplequartet@gmail.com>
2025-10-27 12:06:52 +0900

    feat: Refactor inspector exports for improved organization and clarity

--- src/index.ts
+++ src/index.ts
@@ -93,56 +93,52 @@ export {
   InspectorLayersTab,
   InspectorHistoryTab,
   InspectorSettingsTab,
-} from "./components/inspector/InspectorPanel";
+  NodeInspector,
+  NodeBehaviorInspector,
+  NodeActionsBehaviorInspector,
+  GroupBehaviorInspector,
+  GeneralSettingsPanel,
+  GridSettingsPanel,
+  HistoryPanel,
+  InspectorPropertiesTab,
+  InteractionHelpPanel,
+  NodePalettePanel,
+  NodeTreeListPanel,
+  InspectorSection,
+  PropertySection,
+  InspectorField,
+  InspectorSectionTitle,
+  InspectorLabel,
+  InspectorInput,
+  InspectorNumberInput,
+  InspectorTextarea,
+  InspectorButton,
+  InspectorDefinitionList,
+  InspectorDefinitionItem,
+  InspectorShortcutButton,
+  InspectorShortcutBindingValue,
+  PositionInputsGrid,
+  ReadOnlyField,
+} from "./inspector";
 export type {
   InspectorPanelProps,
   InspectorPanelTabConfig,
   InspectorSettingsPanelConfig,
   InspectorSettingsTabProps,
-} from "./components/inspector/InspectorPanel";
-
-export { NodeInspector } from "./components/inspector/renderers/NodeInspector";
-export type { NodeInspectorProps } from "./components/inspector/renderers/NodeInspector";
-export { NodeBehaviorInspector } from "./components/inspector/renderers/NodeBehaviorInspector";
-export { NodeActionsBehaviorInspector } from "./components/inspector/renderers/NodeActionsBehaviorInspector";
-export { GroupBehaviorInspector } from "./components/inspector/renderers/GroupBehaviorInspector";
-
-export { GeneralSettingsPanel } from "./components/inspector/renderers/GeneralSettingsPanel";
-export { GridSettingsPanel } from "./components/inspector/renderers/GridSettingsPanel";
-export { HistoryPanel } from "./components/inspector/renderers/HistoryPanel";
-export { InspectorPropertiesTab } from "./components/inspector/renderers/InspectorPropertiesTab";
-export { InteractionHelpPanel } from "./components/inspector/renderers/InteractionHelpPanel";
-export { NodePalettePanel } from "./components/inspector/renderers/NodePalettePanel";
-export { NodeTreeListPanel } from "./components/inspector/renderers/NodeTreeListPanel";
-
-export { InspectorSection } from "./components/inspector/parts/InspectorSection";
-export type { InspectorSectionProps } from "./components/inspector/parts/InspectorSection";
-export { PropertySection } from "./components/inspector/parts/PropertySection";
-export type { PropertySectionProps } from "./components/inspector/parts/PropertySection";
-export { InspectorField } from "./components/inspector/parts/InspectorField";
-export type { InspectorFieldProps } from "./components/inspector/parts/InspectorField";
-export { InspectorSectionTitle } from "./components/inspector/parts/InspectorSectionTitle";
-export type { InspectorSectionTitleProps } from "./components/inspector/parts/InspectorSectionTitle";
-export { InspectorLabel } from "./components/inspector/parts/InspectorLabel";
-export type { InspectorLabelProps } from "./components/inspector/parts/InspectorLabel";
-export { InspectorInput } from "./components/inspector/parts/InspectorInput";
-export type { InspectorInputProps } from "./components/inspector/parts/InspectorInput";
-export { InspectorNumberInput } from "./components/inspector/parts/InspectorNumberInput";
-export type { InspectorNumberInputProps } from "./components/inspector/parts/InspectorNumberInput";
-export { InspectorTextarea } from "./components/inspector/parts/InspectorTextarea";
-export type { InspectorTextareaProps } from "./components/inspector/parts/InspectorTextarea";
-export { InspectorButton } from "./components/inspector/parts/InspectorButton";
-export type { InspectorButtonProps } from "./components/inspector/parts/InspectorButton";
-export { InspectorDefinitionList, InspectorDefinitionItem } from "./components/inspector/parts/InspectorDefinitionList";
-export type {
+  NodeInspectorProps,
+  InspectorSectionProps,
+  PropertySectionProps,
+  InspectorFieldProps,
+  InspectorSectionTitleProps,
+  InspectorLabelProps,
+  InspectorInputProps,
+  InspectorNumberInputProps,
+  InspectorTextareaProps,
+  InspectorButtonProps,
   InspectorDefinitionListProps,
   InspectorDefinitionItemProps,
-} from "./components/inspector/parts/InspectorDefinitionList";
-export { InspectorShortcutButton } from "./components/inspector/parts/InspectorShortcutButton";
-export type { InspectorShortcutButtonProps } from "./components/inspector/parts/InspectorShortcutButton";
-export { InspectorShortcutBindingValue } from "./components/inspector/parts/InspectorShortcutBindingValue";
-export type { InspectorShortcutBindingValueProps } from "./components/inspector/parts/InspectorShortcutBindingValue";
-export { PositionInputsGrid } from "./components/inspector/parts/PositionInputsGrid";
-export type { PositionInputsGridProps } from "./components/inspector/parts/PositionInputsGrid";
-export { ReadOnlyField } from "./components/inspector/parts/ReadOnlyField";
-export type { ReadOnlyFieldProps } from "./components/inspector/parts/ReadOnlyField";
+  InspectorShortcutButtonProps,
+  InspectorShortcutBindingValueProps,
+  PositionInputsGridProps,
+  ReadOnlyFieldProps,
+} from "./inspector";
--- src/inspector/index.ts
+++ src/inspector/index.ts
@@ -1,58 +1,68 @@
 /**
  * @file Public inspector exports to help build custom inspector experiences.
+ * This file directly imports from component files to avoid circular dependencies.
  */
+
+// Inspector panel components
 export {
   InspectorPanel,
   InspectorLayersTab,
   InspectorHistoryTab,
   InspectorSettingsTab,
-  NodeInspector,
-  NodeBehaviorInspector,
-  NodeActionsBehaviorInspector,
-  GroupBehaviorInspector,
-  GeneralSettingsPanel,
-  GridSettingsPanel,
-  HistoryPanel,
-  InspectorPropertiesTab,
-  InteractionHelpPanel,
-  NodePalettePanel,
-  NodeTreeListPanel,
-  InspectorSection,
-  PropertySection,
-  InspectorField,
-  InspectorSectionTitle,
-  InspectorLabel,
-  InspectorInput,
-  InspectorNumberInput,
-  InspectorTextarea,
-  InspectorButton,
-  InspectorDefinitionList,
-  InspectorDefinitionItem,
-  InspectorShortcutButton,
-  InspectorShortcutBindingValue,
-  PositionInputsGrid,
-  ReadOnlyField,
-} from "../index";
-
+} from "../components/inspector/InspectorPanel";
 export type {
   InspectorPanelProps,
   InspectorPanelTabConfig,
   InspectorSettingsPanelConfig,
   InspectorSettingsTabProps,
-  NodeInspectorProps,
-  InspectorSectionProps,
-  PropertySectionProps,
-  InspectorFieldProps,
-  InspectorSectionTitleProps,
-  InspectorLabelProps,
-  InspectorInputProps,
-  InspectorNumberInputProps,
-  InspectorTextareaProps,
-  InspectorButtonProps,
+} from "../components/inspector/InspectorPanel";
+
+// Inspector renderer components
+export { NodeInspector } from "../components/inspector/renderers/NodeInspector";
+export type { NodeInspectorProps } from "../components/inspector/renderers/NodeInspector";
+export { NodeBehaviorInspector } from "../components/inspector/renderers/NodeBehaviorInspector";
+export { NodeActionsBehaviorInspector } from "../components/inspector/renderers/NodeActionsBehaviorInspector";
+export { GroupBehaviorInspector } from "../components/inspector/renderers/GroupBehaviorInspector";
+export { GeneralSettingsPanel } from "../components/inspector/renderers/GeneralSettingsPanel";
+export { GridSettingsPanel } from "../components/inspector/renderers/GridSettingsPanel";
+export { HistoryPanel } from "../components/inspector/renderers/HistoryPanel";
+export { InspectorPropertiesTab } from "../components/inspector/renderers/InspectorPropertiesTab";
+export { InteractionHelpPanel } from "../components/inspector/renderers/InteractionHelpPanel";
+export { NodePalettePanel } from "../components/inspector/renderers/NodePalettePanel";
+export { NodeTreeListPanel } from "../components/inspector/renderers/NodeTreeListPanel";
+
+// Inspector part components (form controls and UI elements)
+export { InspectorSection } from "../components/inspector/parts/InspectorSection";
+export type { InspectorSectionProps } from "../components/inspector/parts/InspectorSection";
+export { PropertySection } from "../components/inspector/parts/PropertySection";
+export type { PropertySectionProps } from "../components/inspector/parts/PropertySection";
+export { InspectorField } from "../components/inspector/parts/InspectorField";
+export type { InspectorFieldProps } from "../components/inspector/parts/InspectorField";
+export { InspectorSectionTitle } from "../components/inspector/parts/InspectorSectionTitle";
+export type { InspectorSectionTitleProps } from "../components/inspector/parts/InspectorSectionTitle";
+export { InspectorLabel } from "../components/inspector/parts/InspectorLabel";
+export type { InspectorLabelProps } from "../components/inspector/parts/InspectorLabel";
+export { InspectorInput } from "../components/inspector/parts/InspectorInput";
+export type { InspectorInputProps } from "../components/inspector/parts/InspectorInput";
+export { InspectorNumberInput } from "../components/inspector/parts/InspectorNumberInput";
+export type { InspectorNumberInputProps } from "../components/inspector/parts/InspectorNumberInput";
+export { InspectorTextarea } from "../components/inspector/parts/InspectorTextarea";
+export type { InspectorTextareaProps } from "../components/inspector/parts/InspectorTextarea";
+export { InspectorButton } from "../components/inspector/parts/InspectorButton";
+export type { InspectorButtonProps } from "../components/inspector/parts/InspectorButton";
+export {
+  InspectorDefinitionList,
+  InspectorDefinitionItem,
+} from "../components/inspector/parts/InspectorDefinitionList";
+export type {
   InspectorDefinitionListProps,
   InspectorDefinitionItemProps,
-  InspectorShortcutButtonProps,
-  InspectorShortcutBindingValueProps,
-  PositionInputsGridProps,
-  ReadOnlyFieldProps,
-} from "../index";
+} from "../components/inspector/parts/InspectorDefinitionList";
+export { InspectorShortcutButton } from "../components/inspector/parts/InspectorShortcutButton";
+export type { InspectorShortcutButtonProps } from "../components/inspector/parts/InspectorShortcutButton";
+export { InspectorShortcutBindingValue } from "../components/inspector/parts/InspectorShortcutBindingValue";
+export type { InspectorShortcutBindingValueProps } from "../components/inspector/parts/InspectorShortcutBindingValue";
+export { PositionInputsGrid } from "../components/inspector/parts/PositionInputsGrid";
+export type { PositionInputsGridProps } from "../components/inspector/parts/PositionInputsGrid";
+export { ReadOnlyField } from "../components/inspector/parts/ReadOnlyField";
+export type { ReadOnlyFieldProps } from "../components/inspector/parts/ReadOnlyField";
